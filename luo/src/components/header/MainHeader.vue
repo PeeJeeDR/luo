aedc9dc7b57cdd7697ce885afbec24464971670d
Pieter-Jan De Ridder <pj.pjdr@gmail.com>
2019-04-12 16:03:28 +0200

    COMPONENTS: header init

---
+++ luo/src/components/header/MainHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <div class='wrapper flex justify-between'>
+      <h1>Icon</h1>
+      <h1>Profile</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MainHeader'
+}
+</script>
+
+<style lang='scss' scoped>
+header {
+  @include gradient;
+}
+</style>
