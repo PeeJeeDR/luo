a051b85577d3502607f1e315979d85a766a48c1e
Pieter-Jan De Ridder <pj.pjdr@gmail.com>
2019-05-01 15:58:18 +0200

    LUO-008: icons styled in bottom nav

--- luo/src/components/navigation/BottomNavigation.vue
+++ luo/src/components/navigation/BottomNavigation.vue
@@ -1,38 +1,42 @@
 <template>
   <div class='bottom-navigation flex justify-center align-center'>
-    <div>
-      <button v-ripple class='flex justofy-center align-center'>
-        <Heart />
-      </button>
+    <div class='icon-container flex'>
+      <div>
+        <New />
+      </div>
 
-      <button v-ripple class='flex justofy-center align-center'>
-        <Heart />
-      </button>
+      <div>
+        <Popular />
+      </div>
     </div>
 
     <square-button class='add'>
       <h3 class='heading h--large bright'>+</h3>
     </square-button>
 
-    <div>
-      <button v-ripple class='flex justofy-center align-center'>
-        <Heart />
-      </button>
+    <div class='icon-container flex'>
+      <div>
+        <Star />
+      </div>
 
-      <button v-ripple class='flex justofy-center align-center'>
-        <Heart />
-      </button>
+      <div>
+        <Category />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import SquareButton from '@/components/buttons/SquareButton';
-import Heart from '@/assets/icons/Heart.svg';
+import New from '@/assets/icons/bottom-nav/New.svg';
+import Popular from '@/assets/icons/bottom-nav/Popular.svg';
+import Star from '@/assets/icons/bottom-nav/Star.svg';
+import Category from '@/assets/icons/bottom-nav/Category.svg';
+
 
 export default {
   name: 'BottomNavigation',
-  components: { SquareButton, Heart },
+  components: { SquareButton, New, Popular, Star, Category },
 }
 </script>
 
@@ -59,16 +63,12 @@ export default {
     }
   }
 
-  button {
-    background: none;
-    border: none;
-    border-radius: 50rem;
-    padding: 0.5rem;
 
-    svg {
-      fill: #000;
-      width: 2.5rem;
-    }
+  svg {
+    fill: $dawn;
+    width: 2rem;
+    height: 2rem;
+    margin: 0 1rem;
   }
 }
 </style>
