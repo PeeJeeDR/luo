a9122a280cedb0b6f6b6ee49e06c6f2c08ea274f
Pieter-Jan De Ridder <pj.pjdr@gmail.com>
2019-06-03 17:42:11 +0200

    LUO-002: deploy information

--- luo/src/firebase/firebase.js
+++ luo/src/firebase/firebase.js
@@ -3,18 +3,24 @@ import 'firebase/firestore';
 import 'firebase/auth';
 import 'firebase/storage';
 
-const config = {
+const fire = firebase.initializeApp({
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
   authDomain: process.env.VUE_APP_FIREBASE_DOMAIN,
   databaseURL: process.env.VUE_APP_FIREBASE_DB_URL,
   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.VUE_APP_FIREBASE_BUCKET,
   messagingSenderId: process.env.VUE_APP_FIREBASE_SENDER_ID
-}
+});
 
-console.log('CONFIG', config);
+console.log('ENV', {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DB_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_SENDER_ID
+});
 
-const fire = firebase.initializeApp(config);
 const storage = fire.storage().ref();
 const db = fire.firestore();
 
