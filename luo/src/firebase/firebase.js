1ae71cb380d1a731364654fd78bd94d7eeb6066f
Pieter-Jan De Ridder <pj.pjdr@gmail.com>
2019-06-03 17:11:43 +0200

    LUO-010: moved firebase config to env file

--- luo/src/firebase/firebase.js
+++ luo/src/firebase/firebase.js
@@ -2,9 +2,19 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 import 'firebase/storage';
-import { firebaseConfig } from '@/config/config';
 
-const fire = firebase.initializeApp(firebaseConfig);
+const config = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DB_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_SENDER_ID
+}
+
+console.log('CONFIG', config);
+
+const fire = firebase.initializeApp(config);
 const storage = fire.storage().ref();
 const db = fire.firestore();
 
